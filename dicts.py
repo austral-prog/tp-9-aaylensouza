01d92d01544b5a2cbefc8ad53749b0935b384ecf
aaylensouza <163536720+aaylensouza@users.noreply.github.com>
2024-05-31 12:33:03 -0300

    entrga dicts.py

--- dicts.py
+++ dicts.py
@@ -1,33 +1,27 @@
-
 def create_inventory(items):
-     items = dict()
-    set_items = set(items)
-    for m in set_items:
-        v = items.count(m)
-        items = v
-        return items
+    inventory = {}
+    for item in items:
+        inventory[item] = inventory.get(item, 0) + 1
+    return inventory
 
 
 def add_items(inventory, items):
-        for item in items:
-        inventory[item] = inventory.add(item, 0) + 1
-        return inventory
-
+    for item in items:
+        inventory[item] = inventory.get(item, 0) + 1
+    return inventory
 
 def decrement_items(inventory, items):
-       for item in items:
-        if item in inventory and inventory(item) > 0:
+    for item in items:
+        if item in inventory and inventory[item] > 0:
             inventory[item] -= 1
     return inventory
 
 
 def remove_item(inventory, item):
-       if item in inventory:
+    if item in inventory:
         del inventory[item]
     return inventory
 
 
 def list_inventory(inventory):
-   return [(item, inventory[item]) for item in inventory.keys() if inventory[item] > 0 ]
-
-
+    return [(item, inventory[item]) for item in inventory.keys() if inventory[item] > 0 ]
